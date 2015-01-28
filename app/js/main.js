045e2b50f0358985f9ba5a4688785c6f7c80cb67
Matthew Reuther <matthew.reuther@gmail.com>
2015-01-28 15:30:13 -0600

    clean up

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
-;(function(){
-  'use strict';
+'use strict';
 
-  console.log('NSS Template Loaded');
-}());
+var userZip;
+document.querySelector('#add-userZipInput').addEventListener('click', function(event){
+  userZip = this.previousElementSibling.value;
+  console.log(userZip);
+});
+
+var zipUrl = 'http://api.wunderground.com/api/db432a740561cd8d/forecast10day/q/' + userZip + '.json’;
+
+getJSON(zipUrl, function(result){
+  var forecastData = result['forecast'];
+  });
+
+function getJSON(url, cb) {
+  var forcastData = new XMLHttpRequest();
+  forcastData.open('GET', url);
+
+  forcastData.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      cb(JSON.parse(this.response));
+    }
+  };
+
+  forcastData.send();
+}
+
